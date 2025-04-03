526cb73ffaef43d9da9adfcf8eb16a87271e5860
onepunchkim <hyukjin4711@gmail.com>
2025-04-03 15:48:44 +0900

    lambda start

--- src/lambda/start/Ex1RefMainV4.java
+++ src/lambda/start/Ex1RefMainV4.java
@@ -4,8 +4,8 @@ import lambda.Procedure;
 
 import java.util.Random;
 
-//정적 중첩 클래스 사용
-public class Ex1RefMainV1 {
+//람다 사용
+public class Ex1RefMainV4 {
     public static void hello(Procedure procedure) {
         long startNs = System.nanoTime();
         //코드 조각 시작
@@ -13,34 +13,19 @@ public class Ex1RefMainV1 {
         //코드 조각 종료
         long endNs = System.nanoTime();
         System.out.println("실행 시간: " + (endNs - startNs) + "ns");
-
-    }
-
-
-    static class Dice implements Procedure {
-
-        @Override
-        public void run() {
-            int randomValue = new Random().nextInt(6) + 1;
-            System.out.println("주사위 = " + randomValue);
-        }
-    }
-
-    static class Sum implements Procedure {
-
-        @Override
-        public void run() {
-            for (int i = 1; i <= 3; i++) {
-                System.out.println("i = " + i);
-            }
-        }
     }
 
     public static void main(String[] args) {
-        Procedure dice = new Dice();
-        Procedure sum = new Sum();
 
-        hello(dice);
-        hello(sum);
+        hello(() -> {
+            int randomValue = new Random().nextInt(6) + 1;
+            System.out.println("주사위 = " + randomValue);
+        });
+
+        hello(() -> {
+            for (int i = 1; i <= 3; i++) {
+                System.out.println("i = " + i);
+            }
+        });
     }
 }
