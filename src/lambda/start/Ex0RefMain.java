1a9106404c70faeb4da3ca444a83ff70b7d77943
성스러운 성진이 <hyukjin4711@gmail.com>
2025-04-03 12:40:08 +0900

    lambda start

---
+++ src/lambda/start/Ex0RefMain.java
@@ -0,0 +1,25 @@
+package lambda.start;
+
+public class Ex0RefMain {
+
+    public static void hello() {
+        System.out.println("프로그램 시작"); //변하지 않는 부분
+
+        // 변하는 부분 시작
+        System.out.println("Hello Java");
+        System.out.println("Hello Spring");
+        // 변하는 부분 종료
+        System.out.println("프로그램 종료"); //변하지 않는 부분
+    }
+
+    public static void hello(String str) {
+        System.out.println("프로그램 시작");
+        System.out.println("Hello " + str);
+        System.out.println("프로그램 종료");
+    }
+
+    public static void main(String[] args) {
+        hello("Java");
+        hello("Spring");
+    }
+}
