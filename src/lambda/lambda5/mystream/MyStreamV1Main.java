ccf857235754b168def570a71d774d93c0fc7fc1
성스러운 성진이 <hyukjin4711@gmail.com>
2025-04-08 11:39:44 +0900

    stream

--- src/lambda/lambda5/mystream/MyStreamV1Main.java
+++ src/lambda/lambda5/mystream/MyStreamV1Main.java
@@ -1,5 +1,6 @@
 package lambda.lambda5.mystream;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MyStreamV1Main {
@@ -7,13 +8,22 @@ public class MyStreamV1Main {
         //짝수만 남기고, 남은 값의 2배를 반환
         List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         returnValue(numbers);
+        methodChain(numbers);
     }
 
     private static void returnValue(List<Integer> numbers) {
         MyStreamV1 stream = new MyStreamV1(numbers);
         MyStreamV1 filteredStream = stream.filter(n -> n % 2 == 0);
         MyStreamV1 mappedStream = filteredStream.map(n -> n * 2);
-        List<Integer> result = mappedStream.toList();
-        System.out.println("result = " + result);
+        List<Integer> result1 = mappedStream.toList();
+        System.out.println("result1 = " + result1);
+    }
+
+    private static void methodChain(List<Integer> numbers) {
+        List<Integer> result2 = new ArrayList<>(numbers).stream()
+                .filter(n -> n % 2 == 0)
+                .map(n -> n * 2)
+                .toList();
+        System.out.println("result2 = " + result2);
     }
 }
