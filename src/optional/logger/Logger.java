73338ef467f1e0b03635f8321030d7e62b6de6e2
성스러운 성진이 <hyukjin4711@gmail.com>
2025-04-16 13:53:10 +0900

    optional

--- src/optional/logger/Logger.java
+++ src/optional/logger/Logger.java
@@ -1,5 +1,7 @@
 package optional.logger;
 
+import java.util.function.Supplier;
+
 public class Logger {
     private boolean isDebug = false;
 
@@ -15,6 +17,14 @@ public class Logger {
     public void debug(Object message) {
         if (isDebug) {
             System.out.println("[DEBUG] " + message);
+        }
+    }
+
+    //추가
+    //DEBUG로 설정한 경우만 출력 - 람다를 받아서 실행
+    public void debug(Supplier<?> supplier) {
+        if (isDebug) {
+            System.out.println("[DEBUG] " + supplier.get());
         }
     }
 }
